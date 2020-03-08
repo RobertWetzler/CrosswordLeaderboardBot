edd89929e11880be251f987daa34697d8b0d8fae
Robert Wetzler <rwetzler779@gmail.com>
2020-03-08 17:55:02 -0500

    Added command handler for minTimes

--- bot-script.py
+++ bot-script.py
@@ -565,6 +565,7 @@ def main():
     dp.add_handler(CommandHandler("removeLastTime", removeLastTime))
     dp.add_handler(CommandHandler("averages", averages))
     dp.add_handler(CommandHandler("debugtime", debugtime))
+    dp.add_handler(CommandHandler("best", minTimes))
     # on noncommand i.e message - echo the message on Telegram
     # log all errors
     dp.add_error_handler(error)
