20f2c5082ee1730900593d03705db410d3c353dc
Robert Wetzler <rwetzler779@gmail.com>
2020-04-11 22:41:08 -0500

    Ok this has to make error work

--- bot-script.py
+++ bot-script.py
@@ -64,7 +64,7 @@ def help(update, context):
 def error(update, context):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s". Chat data: %s', update, context.error, context.chat_data)
-    context.bot.send_message(doobieID, context.error.message)
+    context.bot.send_message(doobieID, str(context.error))
 
 
 def reset(update, context):
