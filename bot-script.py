0a0ff50d8c7f4c2ae5cbdd8cfdd7c131e904aa6d
Robert Wetzler <rwetzler779@gmail.com>
2020-05-29 18:06:58 -0500

    Commented out unready changes

--- bot-script.py
+++ bot-script.py
@@ -402,7 +402,7 @@ def dailytimes_job(context):
                     if name in globalChatData[chatID]['streaks']:
                         globalChatData[chatID]['streaks'][name] += 1
                         # Check for longest streak, change admin title
-                        if globalChatData[chatID]['streaks'][name] > globalChatData[chatID]['best_streak']:
+                        """if globalChatData[chatID]['streaks'][name] > globalChatData[chatID]['best_streak']:
                             if 'best_streak_name' not in globalChatData[chatID]:
                                 globalChatData[chatID]['best_streak_name'] = name
                                 admins = context.bot.get_chat_administrators(chatID)
@@ -410,7 +410,11 @@ def dailytimes_job(context):
                                 if admin_id:
                                     context.bot.set
                             elif name != globalChatData[chatID]['best_streak_name']:
-
+                                admins = context.bot.get_chat_administrators(chatID)
+                                admin_id = next((admin.user.id for admin in admins if admin.user.first_name == name),
+                                                None)
+                                if admin_id:
+                                    context.bot.set"""
                     else:
                         globalChatData[chatID]['streaks'][name] = 1
                 total_rank = []
