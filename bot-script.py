ee4ee8b1fc214ca7bf2a7e7a1b50ff32b6e3ea52
EC2 Default User <ec2-user@ip-172-31-46-241.us-east-2.compute.internal>
2021-01-21 16:11:50 +0000

    Added pip requirements

--- bot-script.py
+++ bot-script.py
@@ -34,7 +34,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
-config.admin_id = ***REMOVED***
+config.admin_id = 210183844
 # config.group_id = 's1392971649'
 globalChatData = dict()
 
